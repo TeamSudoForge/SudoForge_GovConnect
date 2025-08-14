626da79200f9ae8d77c7ade28aa5581cb703f865
Sakindu Ransindu <sakindu.ransindu@gmail.com>
2025-08-14 22:55:22 +0530

    feat:department registering and forms

--- backend/src/modules/forms/forms.module.ts
+++ backend/src/modules/forms/forms.module.ts
@@ -17,11 +17,13 @@ import {
 import { FormsService } from './services/forms.service';
 import { FormResponsesService } from './services/form-responses.service';
 import { AnalyticsService } from './services/analytics.service';
+import { DepartmentsService } from './services/departments.service';
 
 // Controllers
 import { FormsController } from './controllers/forms.controller';
 import { FormResponsesController } from './controllers/form-responses.controller';
 import { AnalyticsController } from './controllers/analytics.controller';
+import { DepartmentsController } from './controllers/departments.controller';
 
 @Module({
   imports: [
@@ -40,16 +42,19 @@ import { AnalyticsController } from './controllers/analytics.controller';
     FormsController,
     FormResponsesController,
     AnalyticsController,
+    DepartmentsController,
   ],
   providers: [
     FormsService,
     FormResponsesService,
     AnalyticsService,
+    DepartmentsService,
   ],
   exports: [
     FormsService,
     FormResponsesService,
     AnalyticsService,
+    DepartmentsService,
   ],
 })
 export class FormsModule {}
